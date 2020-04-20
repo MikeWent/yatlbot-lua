b89d80be88f572ad98fc3475c9d81e04d78b762a
Mike_Went <git@meew.me>
2020-04-20 17:34:04 +0300

    Fix token extraction

--- main.lua
+++ main.lua
@@ -3,10 +3,9 @@
 local tg = require 'lib.telegram'
 local utils = require 'lib.utils'
 
--- safely extract token from file, even if it does't exist
 local token = utils.lines_from("token.txt")[1]
 
-if token == {} then
+if token == nil then
     print("Please specify bot token in token.txt")
     os.exit(1)
 end
